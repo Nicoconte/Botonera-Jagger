6e4bab037456ab544434b26b370d16b676c1a5d9
UcraDNA <dantebubb@gmail.com>
2024-03-21 20:01:51 -0300

    dark mode
    my leg

--- index.js
+++ index.js
@@ -3,13 +3,26 @@ let audio = new Audio("./ERES_TONTO.mp3");
 const jaggerQuestion = () => {
     $("#jagger-btn").click(function(e) {
         audio.pause();
-
         audio = new Audio("./ERES_TONTO.mp3");
         audio.preload;
         audio.play();        
     });
 }
 
+$(document).ready(function() {
+    let darkMode = false;
+
+    $("#background-btn").click(function() {
+        darkMode = !darkMode;
+        if (darkMode) {
+            $("body").css("background-color", "#404040");
+            $("#background-btn").text("☀️ Light Mode");
+        } else {
+            $("body").css("background-color", "#FFFFFF");
+            $("#background-btn").text("🌙 Dark Mode");
+        }
+    });
+});
 
 $(document).ready(() => {
     jaggerQuestion();
